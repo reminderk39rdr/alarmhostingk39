9740083c3506fd196bd106ee30af476ff3001384
reminderk39rdr <pinterestke756@gmail.com>
2025-10-10 02:24:59 +0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 # schemas.py
 from pydantic import BaseModel
-from datetime import date
+from datetime import date, datetime
+from typing import Optional
 
 class SubscriptionBase(BaseModel):
     name: str
@@ -12,6 +13,12 @@ class SubscriptionCreate(SubscriptionBase):
 
 class Subscription(SubscriptionBase):
     id: int
+    # Tambahkan field tracking ke schema response
+    reminder_count_h3: int
+    reminder_count_h1: int
+    reminder_count_h0: int
+    last_reminder_time: Optional[datetime]
+    last_reminder_type: Optional[str]
 
     class Config:
         from_attributes = True
