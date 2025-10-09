5b988e8eae3990441a56b22ea4aa295903e44f68
reminderk39rdr <pinterestke756@gmail.com>
2025-10-10 00:14:35 +0700

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import date
+
+class SubscriptionBase(BaseModel):
+    name: str
+    url: str
+    expires_at: date
+
+class SubscriptionCreate(SubscriptionBase):
+    pass
+
+class Subscription(SubscriptionBase):
+    id: int
+
+    class Config:
+        from_attributes = True
