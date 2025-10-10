47db824fc36432baf2847b5f61ab64e392e515f9
reminderk39rdr <pinterestke756@gmail.com>
2025-10-10 20:23:05 +0700

    Update models.py

--- models.py
+++ models.py
@@ -9,12 +9,14 @@ class Subscription(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     url = Column(String)
+    # Kolom baru untuk brand
+    brand = Column(String, index=True)
     expires_at = Column(Date)
-    # Kolom baru untuk tracking reminder
+    # Kolom untuk tracking reminder
     reminder_count_h3 = Column(Integer, default=0) # Jumlah reminder H-3
     reminder_count_h1 = Column(Integer, default=0) # Jumlah reminder H-1
     reminder_count_h0 = Column(Integer, default=0) # Jumlah reminder H-0
     last_reminder_time = Column(DateTime, default=None) # Waktu reminder terakhir dikirim
     last_reminder_type = Column(String, default=None) # Jenis reminder terakhir (h3, h1, h0)
-    # Kolom baru untuk waktu penambahan
+    # Kolom untuk waktu penambahan
     created_at = Column(DateTime, default=datetime.now)
