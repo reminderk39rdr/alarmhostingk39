0130be67e0c2f02a296ce4ae83666fb1e3955244
reminderk39rdr <pinterestke756@gmail.com>
2025-12-03 06:26:50 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,15 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# FIX KHUSUS RENDER PYTHON 3.13 — 100% WORK
 raw_url = os.getenv("DATABASE_URL", "")
 
-# FORCE GANTI KE psycopg (fix Render Python 3.13 bug)
 if raw_url.startswith("postgres://"):
     DATABASE_URL = raw_url.replace("postgres://", "postgresql+psycopg://", 1)
-elif raw_url.startswith("postgresql://"):
-    DATABASE_URL = raw_url.replace("postgresql://", "postgresql+psycopg://", 1)
 else:
-    DATABASE_URL = raw_url  # fallback
+    DATABASE_URL = raw_url
 
 engine = create_engine(
     DATABASE_URL,
