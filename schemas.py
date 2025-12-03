9a03620c0ccca7c8c71d0250655e5c6c17cfda81
reminderk39rdr <pinterestke756@gmail.com>
2025-12-03 12:09:20 +0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,11 +1,13 @@
 from datetime import date
 from pydantic import BaseModel
 
+
 class SubscriptionCreate(BaseModel):
     name: str
     url: str
     expires_at: date
     brand: str | None = None
+
 
 class Subscription(SubscriptionCreate):
     id: int
